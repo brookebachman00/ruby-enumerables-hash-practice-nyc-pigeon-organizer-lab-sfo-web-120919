b449c26566b0b53f09746c5d464156a1f7392aa5
Brooke Bachman <bbachman1994@gmail.com>
2019-11-15 17:53:26 +0000

    Automatically backed up by Learn

--- nyc_pigeon_organizer.rb
+++ nyc_pigeon_organizer.rb
@@ -3,12 +3,12 @@ def nyc_pigeon_organizer(data)
   new_hash = {}
   data.each do |key,value|
     value.each do |baby_key,names|
+      names.each do |name|
     binding.pry
-    new_hash[pigeon] = {
+    new_hash[name] = {
 
     }
+      end
     end
-
   end
-  # write your code here!
 end
